a835129daefcbf4f98eb3b2b5881d4e63332a88f
Esteban1891 <esteban.delahoz15@gmail.com>
2021-09-06 17:20:17 -0500

    update

--- managment_orden/settings.py
+++ managment_orden/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'client',
     'orden',
+    'rest_framework',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
--- managment_orden/urls.py
+++ managment_orden/urls.py
@@ -14,8 +14,35 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+from django.conf import settings 
+from django.conf.urls.static import static
+from django.views.static import serve
+from django.conf.urls import url
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Tecnoglass Form",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.ourapp.com/policies/terms/",
+      contact=openapi.Contact(email="esteban.delahoz15@gmail.com"),
+      license=openapi.License(name="Test License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/api.json', schema_view.without_ui(cache_timeout=0), name='schema-swagger-ui'),
+
 ]
