b52cf156d710f338180c1553f64841961e56a4b1
Esteban1891 <esteban.delahoz15@gmail.com>
2021-09-06 16:02:34 -0500

    update

--- managment_orden/settings.py
+++ managment_orden/settings.py
@@ -124,7 +124,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT = os.path.join('staticfiles')
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 django_heroku.settings(locals())
