0c78843eb915b4570acd5f359015e0530197c823
Esteban1891 <esteban.delahoz15@gmail.com>
2021-09-06 16:32:02 -0500

    update

--- managment_orden/settings.py
+++ managment_orden/settings.py
@@ -129,11 +129,11 @@ USE_TZ = True
 
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'app/staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'app/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'app/static'),
 )
 
 django_heroku.settings(locals())
